5307ab83d8864abe0e1229fcf037b1db5fc0b3b3
rageshm110 <rageshm110@gmail.com>
2016-03-10 19:23:36 +0530

    Databases restored
    Updated with relevant logical names and completed the restore of
    database.

--- Sample Database Configuration.sql
+++ Sample Database Configuration.sql
@@ -11,7 +11,25 @@ RESTORE FILELISTONLY FROM DISK = 'F:\AdventureWorks2014.bak'
 
 -- while restoring specify the new location of data files if required, that is if you want to store the files in a differnt location than default one.
 RESTORE DATABASE AdventureWorks2014
-FROM DISK = 'F:\AdventureWorks2014.bak'
-WITH MOVE '' TO 'F:\MSSQL\Data\.mdf',
-WITH MOVE '_Log' TO 'F:\MSSQL\Data\.ldf'
+FROM DISK = 'F:\AdventureWorks2014.bak' WITH
+MOVE 'AdventureWorks2014_Data' TO 'F:\MSSQL\Data\AdventureWorks2014_Data.mdf',
+MOVE 'AdventureWorks2014_Log' TO 'F:\MSSQL\Data\AdventureWorks2014_Log.ldf',
+STATS = 1
+GO
 
+
+-- AdventureWorksLT DB
+RESTORE FILELISTONLY FROM DISK = 'F:\AdventureWorksLT.bak'
+
+RESTORE DATABASE AdventureWorksLT
+FROM DISK = 'F:\AdventureWorksLT.bak' WITH
+MOVE 'AdventureWorksLT2008_Data' TO 'F:\MSSQL\Data\AdventureWorksLT_Data.mdf',
+MOVE 'AdventureWorksLT2008_Log' TO 'F:\MSSQL\Data\AdventureWorksLT_Log.ldf' ,
+STATS = 1
+GO
+
+sp_helpdb [AdventureWorks2014];
+GO
+
+sp_helpdb [AdventureWorksLT]
+GO
